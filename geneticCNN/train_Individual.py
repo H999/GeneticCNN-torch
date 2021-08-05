6ffde0139c129cb3823e037c90e567f01ebc5182
HUYNH KHANH HOA <hoahk4@fpt.com.vn>
2021-08-05 21:35:09 +0700

    update train_Individual

--- geneticCNN/train_Individual.py
+++ geneticCNN/train_Individual.py
@@ -6,7 +6,7 @@ from tqdm.auto import tqdm
 def train(model, device, train_loader, optimizer, loss_func, epoch):
     model.to(device)
     model.train()
-    pbar = tqdm(train_loader, leave=False, desc="Train Epoch {}".format(epoch))
+    pbar = tqdm(train_loader, leave=False, desc=f"Train Epoch {epoch}")
     for data, target in pbar:
         data, target = data.to(device), target.to(device)
         model.zero_grad()
@@ -15,14 +15,14 @@ def train(model, device, train_loader, optimizer, loss_func, epoch):
         loss = loss_func(output, target)
         loss.backward()
         optimizer.step()
-        pbar.set_postfix({'loss': '{:.6f}'.format(loss.item())})
+        pbar.set_postfix({'loss': '{:.8f}'.format(loss.item())})
 
 
 def test(model, device, test_loader, loss_func, epoch, show=False):
     model.eval()
     test_loss = 0
     correct = 0
-    pbar = tqdm(test_loader, leave=False, desc="Test Epoch {}".format(epoch))
+    pbar = tqdm(test_loader, leave=False, desc=f"Test Epoch {epoch}")
     with torch.no_grad():
         for data, target in pbar:
             data, target = data.to(device), target.to(device)
@@ -34,7 +34,7 @@ def test(model, device, test_loader, loss_func, epoch, show=False):
     test_loss /= len(test_loader.dataset)
 
     if show:
-        print('\nTest Epoch {}: Average loss: {:.4f}, Accuracy: {}/{} ({:.8f}%)\n'.format(
+        print('\nTest Epoch {}: Average loss: {:.8f}, Accuracy: {}/{} ({:.4f}%)\n'.format(
             epoch, test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
 
